1aced8999fe9ee95b22d28baf398d6163de700ff
Avram Korets <abrasha@gmail.com>
2023-07-15 23:17:23 +0300

    another pylint fixes

--- store_parser.py
+++ store_parser.py
@@ -4,8 +4,9 @@ Used to parse all store file formats
 @author: Avi
 """
 import codecs
+import os
 from lxml import objectify
-from tools import save_conf
+from tools import save_conf, load_conf
 
 def get_root(xml_file, encoding):
     """get store xml root, in lxml format"""
@@ -34,3 +35,59 @@ def generate_store_dictionary_lower_case():
     return {'SubChainId':'SubChainID', 'ChainId':'ChainID','LastUpdateDate':'LastUpdateDate',
             'StoreId':'StoreID', 'BikoretNo':'BikoretNo','StoreType':'StoreType',
             'StoreName':'StoreName','Address':'Address', 'City':'City' , 'ZipCode':'ZipCode'}
+
+def get_store_conf_path(chain_name):
+    """get store configuration path"""
+    return f'conf/{chain_name}Store.json'
+
+def analyse_store(conf, file, stores_data, provider_name):
+    """analyse store using given configuration path"""
+    root = get_root(file, conf['encoding'])
+    store_data = {'ProviderName':provider_name}
+    store_count = analyse_store_xml(root, store_data, conf['nameDict'], conf['ignore'], stores_data)
+    return store_count
+
+def analyse_store_xml(root, tag_dict, tag_name_dict, ignore, stores_data):
+    """analyse store by going through the xml"""
+    if root is None:
+        return 0
+
+    have_store_id = False
+    store_count = 0
+
+    for child in root.getchildren():
+        if len(child.getchildren()) > 0:
+            store_count += analyse_store_xml(child, tag_dict, tag_name_dict, ignore, stores_data)
+        else:
+            if child.tag in ignore:
+                continue
+            tag_name = tag_name_dict[child.tag]
+            #print(tag_name, child.tag, child.text)
+            tag_dict[tag_name] = child.text
+            if tag_name == 'StoreID':
+                have_store_id = True
+                #print("")
+
+    if have_store_id:
+        key = f'{tag_dict["ChainID"]}_{tag_dict["SubChainID"]}_{tag_dict["StoreID"]}'
+        stores_data[key] = tag_dict.copy()
+        store_count += 1
+    return store_count
+
+def analyse_store_folder(folder_path, chain_name, stores_data):
+    """returns store count inside the chain name"""
+    total_stores = 0
+    conf_path = get_store_conf_path(chain_name)
+    if os.path.isfile(conf_path):
+        store_conf = load_conf(conf_path)
+        for file in os.listdir(folder_path):
+            if store_conf['ignoreFile'] in file:
+                print(f'ignored file {file}')
+            else:
+                full_path = os.path.join(folder_path, file)
+                store_count = analyse_store(store_conf, full_path, stores_data, chain_name)
+                total_stores += store_count
+                print(f'analysing {full_path} stores:{store_count}')
+    else:
+        print(f'skipping {chain_name}')
+    return total_stores
--- test1.py
+++ test1.py
@@ -1,21 +1,42 @@
-
+from pprint import pprint
+import codecs
+import shutil
 from il_supermarket_scarper.main import ScarpingTask
 from il_supermarket_scarper.scrappers_factory import ScraperFactory
 from il_supermarket_scarper.utils.file_types import FileTypesFilters
+from tools import save_conf
+from store_parser import analyse_store_folder
 
-if __name__ == "__main__":
-    expected = ScraperFactory.all_scrapers_name()
-    output_folder = "data"
-    scrapper_done = ScarpingTask(dump_folder_name=output_folder, only_latest=True, files_types=[FileTypesFilters.STORE_FILE.name], lookup_in_db=False).start()
+def download_store_data(output_folder):
+    """download store data from providers"""
+    scrapper_done = ScarpingTask(dump_folder_name=output_folder, only_latest=True,
+                                    files_types=[FileTypesFilters.STORE_FILE.name]).start()
     print(scrapper_done)
-    folders_from_scraper = list(map(lambda x: x.split("/")[1], scrapper_done))
 
-    time.sleep(5)
-    folders_in_dump_folder = os.listdir(output_folder)
-    folders_in_dump_folder = [
-        name for name in folders_in_dump_folder if not name.startswith(".")
-    ]
-    assert len(folders_in_dump_folder) == len(expected)
-    assert sorted(folders_from_scraper) == sorted(folders_in_dump_folder)
+def create_all_stores_data():
+    """create unified store data inside all_stores.json"""
+    all_scrapers = ScraperFactory.all_scrapers()
+    output_folder = "data"
+    download_store_data(output_folder)
+    #print([repr(s) for s in all_scrapers])
+    scrappers = [s(output_folder) for s in all_scrapers]
+    #mapper_prototype(output_folder)
+
+    stores_data = {}
+    total_stores = 0
+    for scrapper in scrappers:
+        total_stores += analyse_store_folder(scrapper.get_storage_path(),
+                                                scrapper.chain, stores_data)
+        #break
+
+    conf_path = 'conf/all_stores.json'
+    print(f'Save all stores to {conf_path} total_stores:{total_stores}'+
+                            f' uniqueStores:{len(stores_data)}')
+    save_conf(conf_path, stores_data)
+    with codecs.open('all_stores_output.txt', 'w', encoding='utf-16', errors="ignore") as file:
+        pprint(stores_data, file)
 
     shutil.rmtree(output_folder)
+
+if __name__ == "__main__":
+    create_all_stores_data()

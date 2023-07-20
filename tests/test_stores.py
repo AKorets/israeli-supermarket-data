90fe84b9e124ce51555bd1c5e12f33b3f528849f
Avram Korets <abrasha@gmail.com>
2023-07-21 00:39:21 +0300

    pylint fix of test_stores

--- tests/test_stores.py
+++ tests/test_stores.py
@@ -1,6 +1,6 @@
+import re
 from store_parser import download_all_stores
 from tools import load_conf
-import re
 
 def test_online_list():
     download_all_stores()
@@ -12,6 +12,7 @@ def test_online_list():
             url_list.add(re.findall("www.*.il",address.lower())[0])
     expected_urls = {'www.shufersal.co.il', 'www.rami-levy.co.il', 'www.carrefour.co.il',
                      'www.m2000.co.il', 'www.keshet-teamim.co.il', 'www.shukcity.co.il',
-                     'www.edenteva.co.il', 'www.ybitan.co.il', 'www.mega.co.il', 'www.tivtaam.co.il'}
+                     'www.edenteva.co.il', 'www.ybitan.co.il',
+                     'www.mega.co.il', 'www.tivtaam.co.il'}
     difference = url_list ^ expected_urls
     assert not difference

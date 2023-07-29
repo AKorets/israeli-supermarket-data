76b2d7bc5f6a4e5ee9bee7496e6292bb5f39eb09
Avram Korets <abrasha@gmail.com>
2023-07-29 17:40:43 +0300

    pylint fixes

--- store_df_parser.py
+++ store_df_parser.py
@@ -1,13 +1,14 @@
-from il_supermarket_scarper.main import ScarpingTask
-from il_supermarket_scarper.scrappers_factory import ScraperFactory
-from il_supermarket_scarper.utils.file_types import FileTypesFilters
-from tools import save_conf, load_conf
 import glob
 import os
 import shutil
-from xml_parser import get_root, parse_item_xml
-import pandas as pd
 from pathlib import Path
+from pprint import pprint
+import pandas as pd
+from il_supermarket_scarper.main import ScarpingTask
+from il_supermarket_scarper.scrappers_factory import ScraperFactory
+from il_supermarket_scarper.utils.file_types import FileTypesFilters
+from tools import load_conf
+from xml_parser import get_root, parse_item_xml
 
 
 def load_all_stores(data_stores_path = 'data/stores.csv', progress_bar=None):
@@ -19,8 +20,9 @@ def load_all_stores(data_stores_path = 'data/stores.csv', progress_bar=None):
         progress_bar.value = progress_bar.max
     return data_frame
 
-def enumerate_scrapper_with_files(output_folder, ignore_file_dict):
+def enumerate_scrapper_with_files(output_folder, all_stores):
     """usage for scrapper, data_files in enumerate_scrapper_with_files(output_folder)"""
+    ignore_file_dict = all_stores['ignore_file_dict']
     for scrapper_class in ScraperFactory:
         ScarpingTask(dump_folder_name=output_folder, only_latest=True,
                                         files_types=[FileTypesFilters.STORE_FILE.name],
@@ -31,7 +33,11 @@ def enumerate_scrapper_with_files(output_folder, ignore_file_dict):
         data_files = list(file for file in
                             glob.iglob(os.path.join(scrapper.get_storage_path(), pattern)))
         if scrapper.chain in ignore_file_dict:
-            data_files = [file for file in data_files if ignore_file_dict[scrapper.chain] not in file]
+            file_part = ignore_file_dict[scrapper.chain]
+            data_files = [file for file in data_files if file_part not in file]
+        if not data_files:
+            print(f'Failed to find file for scrapper {scrapper.chain}')
+            continue
         yield (scrapper, data_files)
 
 def save_all_stores(store_rows, data_stores_path, tags):
@@ -43,38 +49,44 @@ def save_all_stores(store_rows, data_stores_path, tags):
     data_frame.to_csv(filepath, index=False)
     return data_frame
 
+def parse_item_xml_extension(root, chain_name, all_stores, item_info_dict, store_rows, stop_tag):
+    """wrapper around parse_item_xml, to reduce number of download_all_stores variables"""
+    (tags, ignore, tags_dict) = (all_stores['tags'], all_stores['ignore'], all_stores['tags_dict'])
+    parse_item_xml(root, chain_name, tags, ignore, tags_dict,
+                                item_info_dict, store_rows, stop_tag)
+
+def advance_progress_bar(progress_bar):
+    """handle advance progress bar logic"""
+    if progress_bar:
+        progress_bar.value += 1
+
 def download_all_stores(progress_bar=None, force=False, output_folder = "stores_data",
                         data_stores_path = 'data/stores.csv'):
-    """load or create dataframe based on prices from all providers (from all providers) """
+    """load or create dataframe based on stores from all providers """
     if os.path.isfile(data_stores_path) and not force:
         return load_all_stores(data_stores_path, progress_bar)
 
     #collect store_rows from all providers
     store_rows = []
     all_stores = load_conf('conf/all_df_stores.json')
-    (tags, ignore, tags_dict) = (all_stores['tags'], all_stores['ignore'], all_stores['tags_dict'])
-    (encodings, ignore_file_dict) = (all_stores['encoding'], all_stores['ignore_file_dict'])
+    encodings = all_stores['encoding']
     failed_files = []
-    for scrapper, data_files in enumerate_scrapper_with_files(output_folder, ignore_file_dict):
-        if not data_files:
-            print(f'Failed to find file for scrapper {scrapper.chain}')
-            continue
+    for scrapper, data_files in enumerate_scrapper_with_files(output_folder, all_stores):
         print(scrapper.chain, encodings[scrapper.chain])
         for data_file in data_files:
             root = get_root(data_file, encodings[scrapper.chain])
             if root is None:
-                print('failed to get root of '+data_file)
                 failed_files.append(data_file)
                 continue
             item_info_dict = {}
-            parse_item_xml(root, scrapper.chain, tags, ignore, tags_dict,
+            parse_item_xml_extension(root, scrapper.chain, all_stores,
                                 item_info_dict, store_rows, 'storeid')
 
-        if progress_bar:
-            progress_bar.value += 1
+        advance_progress_bar(progress_bar)
         shutil.rmtree(output_folder)
+
     if failed_files:
         print('failed files:')
         pprint(failed_files)
 
-    return save_all_stores(store_rows, data_stores_path, tags)
+    return save_all_stores(store_rows, data_stores_path, all_stores['tags'])

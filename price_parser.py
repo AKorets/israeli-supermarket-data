215c6d789dfa6f539bdd48b4f45209e998b927a3
Avram Korets <abrasha@gmail.com>
2023-07-31 14:22:09 +0300

    add download_all_promo_prices

--- price_parser.py
+++ price_parser.py
@@ -3,6 +3,8 @@ import glob
 import os
 import shutil
 from pathlib import Path
+from typing import Any
+from dataclasses import dataclass
 from il_supermarket_scarper.scrappers_factory import ScraperFactory
 from il_supermarket_scarper.main import ScarpingTask
 from il_supermarket_scarper.utils.file_types import FileTypesFilters
@@ -12,14 +14,25 @@ from xml_parser import get_root, parse_item_xml
 
 PRICE_ENCODING = 'utf-8-sig'
 
-def enumerate_scrapper_with_files(output_folder):
+@dataclass
+class DownloadTypeData:
+    """Info for ScarpingTask and find the files inside"""
+    pattern: str
+    files_types: Any
+
+def enumerate_scrapper_with_files(output_folder,
+                                    download_type_data:DownloadTypeData=None):
     """usage for scrapper, data_files in enumerate_scrapper_with_files(output_folder)"""
+    if download_type_data is None:
+        pattern = f'{FileTypesFilters.PRICE_FILE.value["should_contain"]}*.xml'
+        files_types = FileTypesFilters.only_price()
+        download_type_data = DownloadTypeData(pattern, files_types)
     for scrapper_class in ScraperFactory:
         ScarpingTask(dump_folder_name=output_folder, only_latest=True,
-                                        files_types=FileTypesFilters.only_price(),
+                                        files_types=download_type_data.files_types,
                                         enabled_scrapers=[scrapper_class],
                                         lookup_in_db=False).start()
-        pattern = f'{FileTypesFilters.PRICE_FILE.value["should_contain"]}*.xml'
+        pattern = download_type_data.pattern
         scrapper = ScraperFactory.get(scrapper_class)(output_folder)
         data_files = list(file for file in
                             glob.iglob(os.path.join(scrapper.get_storage_path(), pattern)))
@@ -43,8 +56,27 @@ def save_all_prices(price_rows, data_prices_path, all_prices):
     data_frame.to_csv(filepath, index=False)
     return data_frame
 
-def download_all_prices(progress_bar=None, force=False, output_folder = "price_data",
-                        data_prices_path = 'data/prices.csv'):
+def download_all_promo_prices(progress_bar=None, force=False, output_folder = "promo_data",
+                        data_prices_path = 'data/promo.csv',
+                        download_type_data:DownloadTypeData=None):
+    """load or create dataframe based on promo from all providers """
+    pattern = f'{FileTypesFilters.PROMO_FILE.value["should_contain"]}*.xml'
+    files_types = FileTypesFilters.only_promo()
+    download_type_data = DownloadTypeData(pattern, files_types)
+    download_all_prices(progress_bar, force, output_folder,
+                        data_prices_path, download_type_data)
+
+def parse_item_xml_extension(root, chain_name, all_prices, item_info_dict,
+                             price_rows, stop_tag):
+    """wrapper around parse_item_xml, to reduce number of download_all_prices variables"""
+    (tags, ignore, tags_dict) = (all_prices['tags'], all_prices['ignore'], all_prices['tags_dict'])
+    parse_item_xml(root, chain_name, tags, ignore, tags_dict,
+                        item_info_dict, price_rows, stop_tag)
+
+def download_all_prices(progress_bar=None, force=False,
+                        output_folder = "price_data",
+                        data_prices_path = 'data/prices.csv',
+                        download_type_data:DownloadTypeData=None):
     """load or create dataframe based on prices from all providers (from all providers) """
     if os.path.isfile(data_prices_path) and not force:
         return load_all_prices(data_prices_path, progress_bar)
@@ -52,9 +84,8 @@ def download_all_prices(progress_bar=None, force=False, output_folder = "price_d
     #collect price_rows from all providers
     price_rows = []
     all_prices = load_conf('conf/all_prices.json')
-    (tags, ignore, tags_dict) = (all_prices['tags'], all_prices['ignore'], all_prices['tags_dict'])
     failed_files = []
-    for scrapper, data_files in enumerate_scrapper_with_files(output_folder):
+    for scrapper, data_files in enumerate_scrapper_with_files(output_folder, download_type_data):
         if not data_files:
             print(f'Failed to find file for scrapper {scrapper.chain}')
             continue
@@ -66,8 +97,8 @@ def download_all_prices(progress_bar=None, force=False, output_folder = "price_d
                 failed_files.append(data_file)
                 continue
             item_info_dict = {}
-            parse_item_xml(root, scrapper.chain, tags, ignore, tags_dict,
-                                item_info_dict, price_rows, 'itemcode')
+            parse_item_xml_extension(root, scrapper.chain, all_prices, item_info_dict,
+                                     price_rows, 'itemcode')
 
         if progress_bar:
             progress_bar.value += 1
